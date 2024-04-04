9854c07d4dbc7d1a6794c77aa60e905e185aab47
AbdelrahmanNijim <nijimabdelrahman@gmail.com>
2024-04-05 02:38:31 +0300

    FinallProject

--- src/Pages/Categories/CategoriesProducts.jsx
+++ src/Pages/Categories/CategoriesProducts.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { Navigate, useParams ,useNavigate } from 'react-router-dom'
+import { Navigate, useParams, useNavigate } from 'react-router-dom'
 import ReactDOM from 'react-dom';
 import { Bounce, Slide, toast } from 'react-toastify';
 import { Puff } from 'react-loader-spinner'
@@ -9,139 +9,142 @@ import styles from "./CategoriesProduct.module.css"
 import ReactStars from 'react-rating-star-with-type'
 function CategoriesProducts() {
   const navigate = useNavigate();
-    const { id } = useParams('id');
-    const [ products, setProducts ] = useState([]);
-    const [isLoading,setLoading]=useState(true);
-    
-    const controller = new AbortController();
-    
-    const getProducts = async () => {
-        try{
+  const { id } = useParams('id');
+  const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
-          const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/products/category/${id}`);
-       console.log(data);
+  const controller = new AbortController();
+
+  const getProducts = async () => {
+    try {
+
+      const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/products/category/${id}`);
+      console.log(data);
       setProducts(data.products);
       setLoading(true);
-        }catch(error){
-          console.log(error);
+    } catch (error) {
+      console.log(error);
 
-        }finally{
-          setLoading(false);
-        }
-        
-
-    };
-    useEffect(() => {
-
-        getProducts();
+    } finally {
+      setLoading(false);
+    }
 
 
-    }, []);
+  };
+  useEffect(() => {
 
-    const addToCart = async(productId)=>{
-      const token =localStorage.getItem("userToken");
-      if (!token) {
-        toast.warn("You Must Login First", {
-            position: "top-center",
-            autoClose: 2000,
+    getProducts();
+
+
+  }, []);
+
+  const addToCart = async (productId) => {
+    const token = localStorage.getItem("userToken");
+    if (!token) {
+      toast.warn("You Must Login First", {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Slide,
+      });
+
+      navigate("/login");
+
+    } else {
+      try {
+        const data = await axios.post(`${import.meta.env.VITE_API_URL}/cart`, {
+          productId
+        }, {
+          headers: {
+            Authorization: `Tariq__${token}`
+          }
+        });
+        console.log(data);
+        setLoading(true);
+        if (data.data.message == 'success') {
+          toast.success('Item Add to the Cart!', {
+            position: "top-right",
+            autoClose: 3000,
             hideProgressBar: true,
             closeOnClick: true,
             pauseOnHover: false,
-            draggable: true,
+            draggable: false,
             progress: undefined,
             theme: "light",
-            transition: Slide,
+            transition: Bounce,
           });
-        
-          navigate("/login");
-        
-    } else{
-             try{
-              const data = await axios.post(`${import.meta.env.VITE_API_URL}/cart`,{
-                  productId
-                },{
-                  headers: {
-                      Authorization: `Tariq__${token}`
-                  }
-                });
-                console.log(data);
-                setLoading(true);
-                if (data.data.message == 'success') {
-                  toast.success('Item Add to the Cart!', {
-                    position: "top-right",
-                    autoClose: 3000,
-                    hideProgressBar: true,
-                    closeOnClick: true,
-                    pauseOnHover: false,
-                    draggable: false,
-                    progress: undefined,
-                    theme: "light",
-                    transition: Bounce,
-                  });
-                }
-                
-            }catch (error){
-              console.log(error)
-              toast.error(error.response.data.message, {
-                position: "top-center",
-                autoClose: 2000,
-                hideProgressBar: true,
-                closeOnClick: true,
-                pauseOnHover: false,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-                transition: Slide,
-              });
-      
-            }finally{
-               setLoading(false);
-            }
-      
-           }
-     
-    };
+        }
+
+      } catch (error) {
+        console.log(error)
+        toast.error(error.response.data.message, {
+          position: "top-center",
+          autoClose: 2000,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Slide,
+        });
+
+      } finally {
+        setLoading(false);
+      }
+
+    }
+
+  };
 
 
 
 
 
-    return (
-        <> 
-       
-           <div><span>If you did not login it will send you to the  log in page when you press on add to cart</span></div>
-        
+  return (
+    <>
 
-          {isLoading ? (
-             <div className='center'>
-              <TailSpin color="red" radius={"20px"} />
-            </div>
-          ) : (
-            products.map((product) => (
-                     
-            <div className={styles.Products} key={product._id}> 
-                 
-               <div className={styles.CardS}>  
-               <div className={styles.cardStart}>  
-               <img src={product.mainImage.secure_url}></img>
-                <span> <ReactStars
-                      value={product.avgRating}
-                      edit={false}
-                      activeColors={["red", "orange", "#FFCE00", "#9177FF", "#8568FC",]}
-                    /></span>
-               </div>
-               
-                <div className={styles.Cardend}>
-                 <div className={styles.details}>  
-                    <h6 className={styles.Titel}>{product.name}</h6>
-                 
-                   <h6>{product.description} </h6>
-                   
-                 </div>
-             
-                     
-                 <div className={styles.AddtoCar}>
-                    <button onClick={ ()=> addToCart  (product._id)} className='btn btn-primary'disabled={isLoading ? 'disabled' : null}
+      <div><span>If you did not login it will send you to the  log in page when you press on add to cart</span></div>
+
+
+      {isLoading ? (
+        <div className='center'>
+          <TailSpin color="red" radius={"20px"} />
+        </div>
+      ) : (
+        products.map((product) => (
+
+          <div className={styles.Products} key={product._id}>
+
+            <div className={styles.CardS}>
+              <div className={styles.cardStart}>
+                <div className={styles.Image}>
+                  <img src={product.mainImage.secure_url}></img>
+                </div>
+                <div className={styles.Rate}> <span> <ReactStars
+                  value={product.avgRating}
+                  edit={false}
+                  activeColors={["red", "orange", "#FFCE00", "#9177FF", "#8568FC",]}
+                /></span></div>
+
+              </div>
+
+              <div className={styles.Cardend}>
+                <div className={styles.details}>
+                  <h2 className={styles.Titel}>{product.name}</h2>
+
+                  <h5>{product.description} </h5>
+
+                </div>
+
+
+                <div className={styles.BTN} >
+                  <a  className={styles.AddtoCar} onClick={() => addToCart(product._id)} disabled={isLoading ? 'disabled' : null}
                   >{!isLoading ? 'Add To cart' : <Puff
                     visible={true}
                     height="40"
@@ -151,17 +154,16 @@ function CategoriesProducts() {
                     wrapperStyle={{}}
                     wrapperClass=""
                   />}
-                  </button>
+                  </a>
 
                 </div>
-                </div>
-               
-               </div>
-                 
-               
-                   
-           </div>
-               
+              </div>
+
+            </div>
+
+
+
+          </div>
 
 
 
@@ -169,20 +171,21 @@ function CategoriesProducts() {
 
 
 
-                
-            
-            
-            
-            
-            
-            ))
 
-            )}
-      
 
-        </>
 
-    )
+
+
+
+
+        ))
+
+      )}
+
+
+    </>
+
+  )
 }
 
 export default CategoriesProducts
--- src/Pages/ForgotPassword/SendCode.jsx
+++ src/Pages/ForgotPassword/SendCode.jsx
@@ -4,154 +4,169 @@ import { object, string } from 'yup';
 import { Bounce, Slide, toast } from 'react-toastify';
 import { Puff } from 'react-loader-spinner'
 import { useNavigate } from 'react-router-dom';
+import styles from './Forgot.module.css'
 
 function SendCode() {
-    const navigate = useNavigate();
-    const [user, setUser] = useState({
-  
-      email: '',
-      
-  
-  
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+
+    email: '',
+
+
+
+  });
+
+  const [errors, setErrors] = useState([]);
+  const [loader, setLoder] = useState(false);
+
+  const handelChange = (e) => {
+    const { name, value } = e.target;
+    setUser({
+      ...user,
+      [name]: value
     });
-  
-    const [errors, setErrors] = useState([]);
-    const [loader, setLoder] = useState(false);
-  
-    const handelChange = (e) => {
-      const { name, value } = e.target;
-      setUser({
-        ...user,
-        [name]: value
-      });
-    };
-  
-  
-    const validateData = async () => {
-      const sendcodeSchema = object({
-        email: string().email().required(),
-  
-      });
+  };
+
+
+  const validateData = async () => {
+    const sendcodeSchema = object({
+      email: string().email().required(),
+
+    });
+    try {
+      await sendcodeSchema.validate(user, { abortEarly: false });
+      return true;
+    } catch (error) {
+      console.log("validation error", error.errors);
+      setErrors(error.errors);
+      setLoder(false);
+      return false;
+    }
+
+  };
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    setLoder(true);
+    const validate = await validateData(user);
+
+    if (await validateData(user)) {
       try {
-        await sendcodeSchema.validate(user, { abortEarly: false });
-        return true;
-      } catch (error) {
-        console.log("validation error", error.errors);
-        setErrors(error.errors);
-        setLoder(false);
-        return false;
-      }
-  
-    };
-  
-    const handelSubmit = async (e) => {
-      e.preventDefault();
-      setLoder(true);
-      const validate = await validateData(user);
-  
-      if (await validateData(user)) {
-        try {
-          const { data } = await axios.patch(`${import.meta.env.VITE_API_URL}/auth/sendcode`, {
-            email: user.email,
-          });
-  
-          setUser({
-            email: '',
-          });
-  
-      
-  
-          if (data.message == 'success') {
-            toast.success('Check Your Email', {
-              position: "top-center",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
-              theme: "light",
-              transition: Bounce,
-            });
-  
-            navigate('/ForgotPassword');
-          }
-  
-        } catch (error) {
-          console.log(error.response);
-          toast.error(error.response.data.message, {
+        const { data } = await axios.patch(`${import.meta.env.VITE_API_URL}/auth/sendcode`, {
+          email: user.email,
+        });
+
+        setUser({
+          email: '',
+        });
+
+
+
+        if (data.message == 'success') {
+          toast.success('Check Your Email', {
             position: "top-center",
-            autoClose: false,
+            autoClose: 5000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
             theme: "light",
-            transition: Slide,
+            transition: Bounce,
           });
-  
-  
-  
-        } finally {
-          setLoder(false);
+
+          navigate('/ForgotPassword');
         }
-  
+
+      } catch (error) {
+        console.log(error.response);
+        toast.error(error.response.data.message, {
+          position: "top-center",
+          autoClose: false,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Slide,
+        });
+
+
+
+      } finally {
+        setLoder(false);
       }
-  
+
     }
-  
-  
-  
-  
-  
-  
-    return (
-      <>
-        <div className='container'>
-          <br />
-          <br />
-          <h2>Forgot the Password ?</h2>
-          <br />
-  
-          {errors.length > 0 ? errors.map(error =>
-            <div> {error}</div>
-          ) : ''}
-  
-  
-          <form onSubmit={handelSubmit}>
-  
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Put Your Email Address To Get The Authentication Code</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={user.email} name='email' onChange={handelChange} />
-              <br />
-  
-            </div>
-            <br />
-  
-            
-  
-            <button type="submit" className="btn btn-primary"
-              disabled={loader ? 'disabled' : null}
-            >{!loader ? 'Send Code' : <Puff
-              visible={true}
-              height="40"
-              width="40"
-              color="#eee"
-              ariaLabel="puff-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-            />}</button>
-          </form>
-        </div>
-  
-      </>
-  
-  
-  
-  
-  
-    )
+
   }
+
+
+
+
+
+
+  return (
+    <>
+      <div className='container'>
+        <br />
+        <br />
+       
+        <br />
+
+        {errors.length > 0 ? errors.map(error =>
+          <div> {error}</div>
+        ) : ''}
+
+
+        <form onSubmit={handelSubmit}>
+          <div className={styles.Form}>
+          <h2>Forgot the Password ?</h2>
+            <div className={styles.row} >
+
+
+              <div className={styles.midForm}>
+
+
+
+                <div className="form-group">
+                <div className={styles.formRow}>
+                  <label htmlFor="exampleInputEmail1">Put Your Email Address To Get The Authentication Code</label>
+                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" value={user.email} name='email' onChange={handelChange} />
+                  <br />
+                  </div>
+                </div>
+                <br />
+
+
+
+
+              </div>
+
+              <button type="submit" className="btn btn-primary"
+                disabled={loader ? 'disabled' : null}
+              >{!loader ? 'Send Code' : <Puff
+                visible={true}
+                height="40"
+                width="40"
+                color="#eee"
+                ariaLabel="puff-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+              />}</button>
+            </div>
+          </div>
+        </form>
+      </div>
+
+    </>
+
+
+
+
+
+  )
+}
 
 export default SendCode
